8af584beb2ab16773e42d97b1befd77b62df5895
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-31 22:44:04 +0100

    Jojo (Add program.py & delete files & French function)

---
+++ program.py
@@ -0,0 +1,16 @@
+def French():
+    print("Bonjour Le monde")
+
+
+def English():
+    print("Hello, World")
+
+
+def main():
+    English()
+    French()
+
+
+if __name__ == "__main__":
+    main()
+
